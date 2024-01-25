02a9dd0d9793f606e57bc0ee8a8ecb5de435746c
vladpotuk <vladpotuk01@gmail.com>
2024-01-25 20:23:06 +0200

    updare 3

--- test/test.dart
+++ test/test.dart
@@ -3,19 +3,16 @@ import 'package:test/test.dart' show equals, expect, group, test;
 void main() {
   group('Sum function tests', () {
     test('Sum of numbers in list', () {
-      // ignore: prefer_typing_uninitialized_variables, non_constant_identifier_names
       var MathFunctions;
       expect(MathFunctions.sum([1, 2, 3, 4]), equals(10));
     });
 
     test('Sum of numbers in empty list', () {
-      // ignore: non_constant_identifier_names, prefer_typing_uninitialized_variables
       var MathFunctions;
       expect(MathFunctions.sum([]), equals(0));
     });
 
     test('Sum of numbers in list with one element', () {
-      // ignore: prefer_typing_uninitialized_variables, non_constant_identifier_names
       var MathFunctions;
       expect(MathFunctions.sum([5]), equals(5));
     });
@@ -23,13 +20,11 @@ void main() {
 
   group('IsEven function tests', () {
     test('Even number', () {
-      // ignore: prefer_typing_uninitialized_variables, non_constant_identifier_names
       var MathFunctions;
       expect(MathFunctions.isEven(4), equals(true));
     });
 
     test('Odd number', () {
-      // ignore: prefer_typing_uninitialized_variables, non_constant_identifier_names
       var MathFunctions;
       expect(MathFunctions.isEven(7), equals(false));
     });
@@ -37,7 +32,6 @@ void main() {
 
   group('UniqueElements function tests', () {
     test('Remove duplicate elements', () {
-      // ignore: prefer_typing_uninitialized_variables, non_constant_identifier_names
       var MathFunctions;
       expect(
           MathFunctions.uniqueElements([1, 2, 3, 3, 4, 5, 5, 6]), equals([1, 2, 3, 4, 5, 6]));
@@ -46,7 +40,6 @@ void main() {
 
   group('SortNumbers function tests', () {
     test('Sort list of numbers', () {
-      // ignore: prefer_typing_uninitialized_variables, non_constant_identifier_names
       var MathFunctions;
       expect(MathFunctions.sortNumbers([3, 2, 1]), equals([1, 2, 3]));
     });
@@ -54,13 +47,11 @@ void main() {
 
   group('Factorial function tests', () {
     test('Factorial of 5', () {
-      // ignore: prefer_typing_uninitialized_variables, non_constant_identifier_names
       var MathFunctions;
       expect(MathFunctions.factorial(5), equals(120));
     });
 
     test('Factorial of 0', () {
-      // ignore: prefer_typing_uninitialized_variables, non_constant_identifier_names
       var MathFunctions;
       expect(MathFunctions.factorial(0), equals(1));
     });
@@ -68,25 +59,21 @@ void main() {
 
   group('CountVowels function tests', () {
     test('Count vowels in string', () {
-      // ignore: prefer_typing_uninitialized_variables, non_constant_identifier_names
       var MathFunctions;
       expect(MathFunctions.countVowels("Hello World"), equals(3));
     });
 
     test('Count vowels in empty string', () {
-      // ignore: prefer_typing_uninitialized_variables, non_constant_identifier_names
       var MathFunctions;
       expect(MathFunctions.countVowels(""), equals(0));
     });
 
     test('Count vowels in string with no vowels', () {
-      // ignore: prefer_typing_uninitialized_variables, non_constant_identifier_names
       var MathFunctions;
       expect(MathFunctions.countVowels("Brrr"), equals(0));
     });
 
     test('Count vowels in string with uppercase letters', () {
-      // ignore: prefer_typing_uninitialized_variables, non_constant_identifier_names
       var MathFunctions;
       expect(MathFunctions.countVowels("AaEeIiOoUu"), equals(10));
     });
